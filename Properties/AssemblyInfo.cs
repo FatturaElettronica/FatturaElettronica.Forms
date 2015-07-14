20e865f5e2b1550d91bde30a0b85c34193a0e131
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-07-14 10:14:48 +0200

    Upgrade BusinessObjects/FatturaElettronica dependencies

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CIR 2000")]
 [assembly: AssemblyProduct("FatturaElettronicaPA.Forms")]
-[assembly: AssemblyCopyright("Copyright © CIR 2000 2014")]
+[assembly: AssemblyCopyright("Copyright © CIR 2000 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
