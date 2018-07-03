b05f95363934060f394cea6721c20a3b9e4008db
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-07-03 15:20:12 +0200

    Bump version to 0.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicola Iarocci, CIR2000")]
 [assembly: AssemblyProduct("FatturaElettronica.Forms")]
-[assembly: AssemblyCopyright("Copyright © CIR2000 2017")]
+[assembly: AssemblyCopyright("Copyright © CIR2000 2017-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.2.0")]
-[assembly: AssemblyFileVersion("0.7.2.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
