0e3841064d1f5e3500b10bb85c3384ebc9f5ca4b
Nicola Iarocci <nicola@nicolaiarocci.com>
2021-01-08 15:17:41 +0100

    bump version to 3.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicola Iarocci, CIR2000")]
 [assembly: AssemblyProduct("FatturaElettronica.Forms")]
-[assembly: AssemblyCopyright("Copyright © CIR2000 2017-2019")]
+[assembly: AssemblyCopyright("Copyright © CIR2000 2017-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.2.0")]
-[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
